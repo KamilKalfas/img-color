21526f62ea081b1e89947393a5fc9155949181e8
Kamil Kalfas <kamilkalfas@gmail.com>
2017-07-03 14:31:40 +0100

    dominant color v1

---
+++ index.js
@@ -0,0 +1,22 @@
+const temp = require('temp').track();
+const	download = require('download');
+const dominantColor = require('dominant-color');
+
+module.exports = {
+	getDominantColor: url => {
+		return new Promise((resolve, reject) => {
+			const stream = temp.createWriteStream();
+			download(url)
+				.then(data => {
+					stream.write(data);
+					stream.end();
+					dominantColor(stream.path, (err, c) => {
+						if (err) {
+							reject(err);
+						}
+						resolve({dColor: c});
+					});
+				});
+		});
+	}
+};
